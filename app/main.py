d155ea065baae1f6dd2ece67e734bb3f20e39a76
Askhat <askhat.ss23@gmail.com>
2025-04-10 18:18:53 +0500

    deploy

--- app/main.py
+++ app/main.py
@@ -46,4 +46,4 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.PORT, reload=True)
